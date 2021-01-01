d4e17cb65409cd8fd07107c8eb59cb44cf80f97f
AhmadiDev21 <AhmadiDev21@gmail.com>
2021-01-01 02:49:06 -0800

    simple skeleton

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
     <title>VideoTube</title>
 </head>
 <body>
+<div id="pageContainer">
+    <div id="mastHeadContainer">
+        <button class="navShowHide" >click me</button>
+    </div>
+    <div id="sideNavContainer">
 
+    </div>
+    <div id="mainSectionContainer">
+        <div id="mainContainContainer">
+            hello everyone
+        </div>
+    </div>
+</div>
 </body>
 </html>
