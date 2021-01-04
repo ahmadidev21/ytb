e409aca85ca6b915df49378a0c38a4895eced322
AhmadiDev21 <AhmadiDev21@gmail.com>
2021-01-04 13:53:05 -0800

    Checking the size of the uploaded video

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -1,12 +1,14 @@
 <?php
+
 require_once("includes/header.php");
 require_once("includes/classes/VideoUploadData.php");
 require_once("includes/classes/VideoProcessor.php");
 
-
 class VideoProcessor
 {
     private $con;
+
+    private $sizeLimit = 500000000;
 
     public function __construct($con)
     {
@@ -18,12 +20,30 @@ class VideoProcessor
     {
         $targetDir = "uploads/videos/";
         $videoData = $videoUploadData->videoDataArray;
-        
-        $tempFilePath = $targetDir. uniqid() . basename($videoData["name"]);
+
+        $tempFilePath = $targetDir . uniqid() . basename($videoData["name"]);
 
         $tempFilePath = str_replace(" ", "_", $tempFilePath);
 
-        echo $tempFilePath;
+        $isValidData = $this->processData($videoData, $tempFilePath);
+//        echo $tempFilePath;
+    }
+
+    private function processData($videoData, $filePath)
+    {
+        $videoType = pathinfo($filePath, PATHINFO_EXTENSION);
+        if (! $this->isValidSize($videoData)) {
+            echo "File Too Large. Can't more be than" . $this->sizeLimit."bytes.";
+
+            return false;
+        }
+        echo $videoType;
+    }
+
+    private function isValidSize($data)
+    {
+        return $data["size"] < $this->sizeLimit;
     }
 }
+
 ?>
